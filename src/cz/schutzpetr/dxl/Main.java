32e96affb2e6574ed3281b18fad3d1f51540d8bd
Petr Schütz <petr.schutz@unicorn.eu>
2017-12-02 22:31:05 +0100

    Better file reader

--- src/cz/schutzpetr/dxl/Main.java
+++ src/cz/schutzpetr/dxl/Main.java
@@ -3,6 +3,8 @@ package cz.schutzpetr.dxl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,11 +13,9 @@ public class Main {
 
         Scanner scanner = new Scanner(new File("test-data.txt"));
 
-        int[][] tosolve = new int[809][82];
+        List<String[]> tosolve = new ArrayList<>();
 
-        int count = 0;
-
-        while (scanner.hasNextLine()){
+        while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             line = line.replace("{", "");
             line = line.replace("}", "");
@@ -23,18 +23,22 @@ public class Main {
             line = line.replace("]", "");
 
             String[] lines = line.split(",");
-            int[] parsed = new int[lines.length];
-
-            for (int i = 0; i < lines.length; i++) {
-                try{
-                    parsed[i] = Integer.parseInt(lines[i].trim());
-                } catch (NumberFormatException e){
-                    e.printStackTrace();
-                }
-            }
-            tosolve[count] = parsed;
-            count++;
+            tosolve.add(lines);
         }
+
+        int[][] matrix = new int[tosolve.size()][];
+
+        for (int i = 0; i < tosolve.size(); i++) {
+            String[] row = tosolve.get(i);
+            int[] parsed = new int[row.length];
+
+            for (int j = 0; j < row.length; j++) {
+                parsed[j] = Integer.parseInt(row[j].trim());
+            }
+
+            matrix[i] = parsed;
+        }
+
 
         int[][] example = {
                 {0, 0, 1, 0, 1, 1, 0},
@@ -54,7 +58,7 @@ public class Main {
                 {0, 0, 1, 0}
         };
 
-        DancingLinks dancingLinks = new DancingLinks(tosolve);
+        DancingLinks dancingLinks = new DancingLinks(matrix);
         dancingLinks.run();
     }
 }
